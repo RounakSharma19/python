23b1147dc0fa413c5278aa3943338972029ca5d3
RounakSharma19 <rounaksharma808@gmail.com>
2025-08-18 21:47:32 +0530

    strings in progress

---
+++ strings.py
@@ -0,0 +1,30 @@
+# name = "Rounak"
+# print("Hello," + name)
+name1 = ''' Hi Rounak
+Hi Siri!! How are you?
+I am great Rounak, Thanks for asking '''
+
+print(name1)
+
+
+print("Lets use a for loop with strings\n")
+for character in name1 :
+    # print(character)
+
+
+    # slicing in string
+
+    fruit = "Mango"
+    mangoLen = len(fruit)
+    print(mangoLen)
+    print(fruit[0:4])
+    print(fruit[1:4])
+    print(fruit[:5])
+    print(fruit[:])
+    print(fruit[0: -3])
+    print(fruit[0:len(fruit) -3])
+    print(fruit[-3: -1])
+
+
+    nm="Harry"
+    print(nm[-4:-2])
